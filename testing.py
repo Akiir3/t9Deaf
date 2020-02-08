acaf81f3e43ead393391e1123004f5a5cae6b6b2
Maxwell Wenzel <maxwell.wenzel@nordstrom.com>
2020-02-07 21:19:45 -0700

    Added a git ignore and test file

---
+++ testing.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+if __name__ == '__main__':
+    cap = cv.VideoCapture('./data/test.mov')
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv.destroyAllWindows()
